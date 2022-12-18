fafbcd01915a44ff0278fb856270cba1d3d41150
Franccesco Michael Jaimes Agreda <70769418+Franccesco1907@users.noreply.github.com>
2022-12-18 05:23:16 -0500

    Adding controller and service in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CommitController } from './commit/commit.controller';
+import { CommitService } from './services/commit.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CommitController],
+  providers: [AppService, CommitService],
 })
 export class AppModule {}
